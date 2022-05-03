abe5c4bd8fc4d7bd513265999eea804086b8d61e
binaryleo <leomouracss@fastmail.com>
2022-05-03 16:49:46 -0300

    feat: widgetForm buttons

--- web/src/components/WidgetForm.tsx
+++ web/src/components/WidgetForm.tsx
@@ -37,7 +37,11 @@ export function WidgetForm() {
              {
                 Object.entries(feedbackTypes).map(([key,value])=>{
                     return(
-                       <button>
+                       <button
+                       className="bg-zinc-800 rounded py-5 w-24 flex1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
+                       type="button"
+                       //onClick={}
+                       >
                            <img src={value.image.source} alt={value.image.alt}></img>
                            <span>{value.title}</span>
                        </button> 
