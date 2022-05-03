e65336afd191844db0ed2ce85379dc4b4d752376
Binaryleo <leomouracss@fastmail.com>
2022-05-02 22:33:12 -0300

    package: tailwindcss initial setup

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'// core components
 import ReactDOM from 'react-dom/client'// integration components
 import App from './App'// Main function
-
+import './global.css'// global styles
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
