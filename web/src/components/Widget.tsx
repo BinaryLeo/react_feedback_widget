81f3b4c258bedc1d68a232bc96924c7ae42fa1cc
binaryleo <leomouracss@fastmail.com>
2022-05-03 15:03:06 -0300

    feat: WidgetForm component

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -1,9 +1,10 @@
 import { ChatTeardropDots } from 'phosphor-react'
 import { Popover } from '@headlessui/react'
+import { WidgetForm } from './WidgetForm'
 export function Widget() {
   return (
     <Popover className="absolute bottom-5 right-5">
-      <Popover.Panel>Widget ok</Popover.Panel>
+      <Popover.Panel><WidgetForm/></Popover.Panel>
       <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
         {/* create a group */}
         <ChatTeardropDots className="w-6 h-6" />
---
+++ web/src/components/WidgetForm.tsx
@@ -0,0 +1,5 @@
+export function WidgetForm(){
+    return(
+        <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg">Say hi test!</div>
+    )
+}
