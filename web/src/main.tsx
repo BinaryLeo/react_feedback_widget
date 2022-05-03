37b91fd4adfd68e1a7087cef6c9b084b2d9d8d47
Binaryleo <leomouracss@fastmail.com>
2022-05-02 21:17:15 -0300

    remove: delete css files

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react'// core components
+import ReactDOM from 'react-dom/client'// integration components
+import App from './App'// Main function
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
