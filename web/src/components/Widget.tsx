8c50b19735d29f948c4bc60fdbd8b88e709ec509
binaryleo <leomouracss@fastmail.com>
2022-05-03 15:05:45 -0300

    feat: Widget position changed

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -3,7 +3,7 @@ import { Popover } from '@headlessui/react'
 import { WidgetForm } from './WidgetForm'
 export function Widget() {
   return (
-    <Popover className="absolute bottom-5 right-5">
+    <Popover className="absolute bottom-5 right-5 flex flex-col items-end">
       <Popover.Panel><WidgetForm/></Popover.Panel>
       <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
         {/* create a group */}
