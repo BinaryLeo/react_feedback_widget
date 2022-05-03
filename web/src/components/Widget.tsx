3e6461c96be0ec164ea0c637d6c83b31e35fb318
Binaryleo <leomouracss@fastmail.com>
2022-05-03 00:09:54 -0300

    feat: add accessibility for Wiget using headless UI

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -1,18 +1,10 @@
-import { useState } from 'react'
 import { ChatTeardropDots } from 'phosphor-react'
+import { Popover } from '@headlessui/react'
 export function Widget() {
-  const [isWidgetOpen, setIsWidgetOpen] = useState(false)
-  function toggleWidgetVisibility() {
-    setIsWidgetOpen(!isWidgetOpen)
-  }
   return (
-    <div className="absolute bottom-5 right-5">
-      {isWidgetOpen && <p>Widget ok</p>}
-
-      <button
-        onClick={toggleWidgetVisibility}
-        className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group"
-      >
+    <Popover className="absolute bottom-5 right-5">
+      <Popover.Panel>Widget ok</Popover.Panel>
+      <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
         {/* create a group */}
         <ChatTeardropDots className="w-6 h-6" />
         <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
@@ -21,7 +13,7 @@ export function Widget() {
           <span className="pl-2"></span>
           Feedback
         </span>
-      </button>
-    </div>
+      </Popover.Button>
+    </Popover>
   )
 }
