65cc57c67c986493aa338154579b3a61a8fae8ee
binaryleo <leomouracss@fastmail.com>
2022-05-04 11:22:09 -0300

    feat: Add feedback steps components

--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -1,7 +1,7 @@
-import { CloseButton } from "./CloseButton";
-import bugImageUrl from '../assets/bug.svg';
-import ideaImageUrl from '../assets/idea.svg';
-import thoughtImageUrl from '../assets/thought.svg';
+import { CloseButton } from "../CloseButton";
+import bugImageUrl from '../../assets/bug.svg';
+import ideaImageUrl from '../../assets/idea.svg';
+import thoughtImageUrl from '../../assets/thought.svg';
 import { useState } from "react";
 
 const feedbackTypes = {
