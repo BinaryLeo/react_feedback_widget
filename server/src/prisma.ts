e086b843d1fad862442523a41c3c30077c653c31
Binaryleo <leomouracss@fastmail.com>
2022-05-05 23:47:57 -0300

    feat: post feeback - route

---
+++ server/src/prisma.ts
@@ -0,0 +1,6 @@
+import { PrismaClient } from "@prisma/client";
+
+export const prisma = new PrismaClient({
+    log:['query']// show in console when a query is executed;
+    
+});
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,19 @@
 import express from 'express';
+import { prisma } from './prisma';
+import { StatusCodes } from 'http-status-codes';
 const app = express();
 app.use(express.json());
-// A middleware that comes first - use to avoid the error: Cannot read property 'json' of undefined
+//* A middleware that comes first - use to avoid the error: Cannot read property 'json' of undefined.
 
-app.post('/feedbacks',(req,res)=>{
-console.log(req.body)
-return res.send('Hello leo theres no bug n this time')
-
+app.post('/feedbacks', async (req, res) => {
+  const { type, comment, screenshot } = req.body //*destructuring
+  const feedback = await prisma.feedback.create({
+    //* await for the feedback to be created ... then ...
+    data: { type, comment, screenshot },
+  })
+  return res.status(StatusCodes.OK).json({ data: feedback });
+   //* ...  when .. created ←  →  return the response.
 })
-app.listen(3333,() => {
-    console.log('Server is running on port 3333');
-});
+app.listen(3333, () => {
+  console.log('Server is running on port 3333');
+})
