502f9cc2946625e9e05a3048de933aa6fcfe7777
Binaryleo <leomouracss@fastmail.com>
2022-05-05 23:06:05 -0300

    chore: express json support

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,12 @@
 import express from 'express';
 const app = express();
-app.get('/users',(req,res)=>{
-return res.send('Hello')
+app.use(express.json());
+// A middleware that comes first - use to avoid the error: Cannot read property 'json' of undefined
+
+app.post('/feedbacks',(req,res)=>{
+console.log(req.body)
+return res.send('Hello leo theres no bug n this time')
+
 })
 app.listen(3333,() => {
     console.log('Server is running on port 3333');
