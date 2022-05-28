9963a0c075581637a1491be545813511ea349e50
binaryleo <leomouracss@fastmail.com>
2022-05-27 23:54:22 -0300

    env:config to deploy

--- web/lib/api.ts
+++ web/lib/api.ts
@@ -1,4 +1,6 @@
 import axios from 'axios';
+
 export const api = axios.create({
-    baseURL: 'http://localhost:3333'
+   // baseURL: 'http://localhost:3333'
+   baseURL: import.meta.env.VITE_API_URL
 }); 
