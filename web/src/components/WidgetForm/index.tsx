51f15ef69433f0806b3de6be651af9578bcc8789
binaryleo <leomouracss@fastmail.com>
2022-05-28 00:31:25 -0300

    fix: types atribute

--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -40,7 +40,7 @@ export function WidgetForm() {
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
       {feedbackSent ? (
-        <FeedbackSuccessStep onFeedbackRestartRequest={handleRestartFeedback} />
+        <FeedbackSuccessStep onFeedbackRestartRequested={handleRestartFeedback} />
       ) : (
         <>
           {!feedbackType ? (
