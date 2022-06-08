60004a4be8876486b82d03be28994432a26abcd7
Binary Leo <leomouracss@fastmail.com>
2022-06-08 14:41:16 -0300

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,6 +4,11 @@ import { routes } from './routes';
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use(routes);
 app.listen(process.env.PORT || 3333, () => {
   console.log('Server is running on port 3333');
