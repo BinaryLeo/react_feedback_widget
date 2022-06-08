d360c4190459e766270a70b64be4b5549e5c7e5f
binaryleo <leomouracss@fastmail.com>
2022-06-07 23:36:58 -0300

    chore: server ts config

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,6 +5,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {
   console.log('Server is running on port 3333');
 })
