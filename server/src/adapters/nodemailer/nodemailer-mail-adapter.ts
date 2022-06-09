9fad385d43a658ef4c4297339a6927d950eebd86
Binary Leo <leomouracss@fastmail.com>
2022-06-08 22:58:33 -0300

    chore: "update credentials"

--- server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -1,13 +1,13 @@
 import { MailAdapter, SendMailData } from "../mail-adapter";
 import nodemailer from "nodemailer";
 const transport = nodemailer.createTransport({
-  host: process.env.SMTP,
-  port: 2525,
-  auth: {
-    user: process.env.USERMAIL,
-    pass: process.env.USERPASS,
-  },
-});
+    host: 'smtp.mailtrap.io',
+    port: 2525,
+    auth: {
+      user: '248ffa2a207505',
+      pass: '70a86870e09ca5',
+    },
+  });
 export class NodemailerMailAdapter implements MailAdapter {
   async sendMail({ subject, body }: SendMailData) {
     await transport.sendMail({
