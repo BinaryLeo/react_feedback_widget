97462a336072b54d3a603a4e8b41fafa1d6246d4
Binary Leo <leomouracss@fastmail.com>
2022-06-20 23:09:40 -0300

    chore: fix payload size

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,7 +5,7 @@ import { routes } from './routes';
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 app.use(routes);
 
 app.listen(process.env.PORT || 3333, () => {
